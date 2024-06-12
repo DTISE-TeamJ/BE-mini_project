40f88ebb5236803d3ccd17c933f41e49caeb9a28
irwinekabakti <irwinekabakti@gmail.com>
2024-06-12 18:47:23 +0700

    initial commit

---
+++ src/main/java/com/example/BE_mini_project/BeMiniProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.BE_mini_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeMiniProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeMiniProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BE_mini_project/BeMiniProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BE_mini_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeMiniProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
