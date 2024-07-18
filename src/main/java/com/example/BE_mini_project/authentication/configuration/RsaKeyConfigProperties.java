5cdc5dbccdb67a5856153e56b81c0b2de97bbcf9
irwinekabakti <irwinekabakti@gmail.com>
2024-07-18 11:40:54 +0700

    fix:(setup jwt for production)

--- src/main/java/com/example/BE_mini_project/authentication/configuration/RsaKeyConfigProperties.java
+++ src/main/java/com/example/BE_mini_project/authentication/configuration/RsaKeyConfigProperties.java
@@ -1,3 +1,35 @@
+package com.example.BE_mini_project.authentication.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+@Component
+@ConfigurationProperties(prefix = "rsa")
+public class RsaKeyConfigProperties {
+    private RSAPublicKey publicKey;
+    private RSAPrivateKey privateKey;
+
+    public RSAPublicKey getPublicKey() {
+        return publicKey;
+    }
+
+    public void setPublicKey(RSAPublicKey publicKey) {
+        this.publicKey = publicKey;
+    }
+
+    public RSAPrivateKey getPrivateKey() {
+        return privateKey;
+    }
+
+    public void setPrivateKey(RSAPrivateKey privateKey) {
+        this.privateKey = privateKey;
+    }
+}
+
 /*
 package com.example.BE_mini_project.authentication.configuration;
 
